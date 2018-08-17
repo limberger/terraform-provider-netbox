881882a168387899337a81a9cb984601418e1cf3
F4955639 Joao Paulo Limberger <f4955639@bb.com.br>
2018-08-17 11:06:12 -0300

    adjust output file names

--- samplenetbox.tf
+++ samplenetbox.tf
@@ -7,32 +7,17 @@ data "netbox_prefixes" "prefixes" {
 	prefixes_id = 1
 }
 
+output "out_prefixes" {
+	value_prefixes_id = "${netbox_prefixes.prefixes.prefixes_id}"
+	value_created = "${netbox_prefixes.prefixes.created}"
+	value_description = "${netbox_prefixes.prefixes.description}"
+	value_family = "${netbox_prefixes.prefixes.family}"
+    value_id = "${netbox_prefixes.prefixes.id}"
+    value_is_pool = "${netbox_prefixes.prefixes.ispool}"
+    value_last_updated = "${netbox_prefixes.prefixes.last_updated}"
+
+}
+
 data "netbox_first_free_address" "next_address" {
 	prefixes_id = "${data.netbox_prefixes.prefixes.prefixes_id}"
 }
-
-
-
-#data "netbox_subnet" "subnet" {
-#  subnet_address = "10.10.2.0"
-#  subnet_mask    = 24
-#}
-
-
-#data "netbox_first_free_address" "next_address" {
-#  subnet_id = "${data.netbox_subnet.subnet.subnet_id}"
-#}
-
-#resource "netbox_address" {
-#	subnet_id = "${data.netbox_subnet.subnet.subnet_id}"
-#	ip_address = "${data.netbox_first_free_address.next_address.ip_address}"
-#	hostame = "tf-test-host.io.bb.com.br"
-#	description = "Managed by Terraform"
-#
-#	lifecycle {
-#		ignore_changes = [
-#			"subnet_id",
-#			"ip_address",
-#		]
-#	}
-#}
