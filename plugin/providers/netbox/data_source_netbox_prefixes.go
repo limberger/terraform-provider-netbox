79c3777a17005387c49334929d480c0c46350d3a
F4955639 Joao Paulo Limberger <f4955639@bb.com.br>
2018-08-17 10:17:53 -0300

    initial commit

--- plugin/providers/netbox/data_source_netbox_prefixes.go
+++ plugin/providers/netbox/data_source_netbox_prefixes.go
@@ -103,4 +103,64 @@ func dataSourceNetboxPrefixesRead(d *schema.ResourceData, meta interface{}) erro
 }
 
 
+
+
+func barePrefixesSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"prefixes_id": &schema.Schema{
+			Type: schema.TypeInt,
+		},
+	}
+}
+
  
+ func resourcePrefixesSchema() map[string]*schema.Schema {
+	s := barePrefixesSchema()
+
+	for k, v := range s {
+		switch k {
+		case "prefixes_id":
+			v.Optional = true
+			v.Computed = true
+			//v.ConflictsWith = []string{"ip_address", "subnet_id", "description", "hostname", "custom_field_filter"}
+		default:
+			v.Computed = true
+		}
+	}
+	// Add the remove_dns_on_delete item to the schema. This is a meta-parameter
+	// that is not part of the API resource and exists to instruct PHPIPAM to
+	// gracefully remove the address from its DNS integrations as well when it is
+	// removed. The default on this option is true.
+	s["remove_dns_on_delete"] = &schema.Schema {
+		Type: schema.TypeBool,
+		Optional: true,
+		Default: true,
+	}
+	return s
+
+}
+
+// dataSourceAddressSchema returns the schema for the phpipam_address data
+// source. It sets the searchable fields and sets up the attribute conflicts
+// between IP address and address ID. It also ensures that all fields are
+// computed as well.
+func dataSourcePrefixesSchema() map[string]*schema.Schema {
+	s := barePrefixesSchema()
+	for k, v := range s {
+		switch k {
+		case "prefixes_id":
+			log.Printf("[DEBUG] JP dataSourcePrefixesSchema()\n")
+			v.Optional = true
+			v.Computed = true
+			//v.ConflictsWith = []string{"ip_address", "subnet_id", "description", "hostname", "custom_field_filter"}
+		default:
+			v.Computed = true
+		}
+	}
+	// Add the custom_field_filter item to the schema. This is a meta-parameter
+	// that allows searching for a custom field value in the data source.
+	s["custom_field_filter"] = customFieldFilterSchema([]string{"prefixes_id"})
+
+	return s
+}
+
--- plugin/providers/netbox/provider.go
+++ plugin/providers/netbox/provider.go
@@ -7,54 +7,84 @@ import (
 	"github.com/hashicorp/terraform/terraform"
 )
 
-// Provider returns a terraform.ResourceProvider.
-func Provider() terraform.ResourceProvider {
-	return &schema.Provider{
-		Schema: map[string]*schema.Schema{
-			"app_id": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "",
-				Description: descriptions["app_id"],
-			},
-			"endpoint": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "",
-				Description: descriptions["endpoint"],
-			},
-		},
-
-		ResourcesMap: map[string]*schema.Resource{
-//			"netbox_address": resourceNetboxAddress(),
-		    "netbox_prefixes": resourceNetboxPrefixes(),
-		},
-
-		DataSourcesMap: map[string]*schema.Resource{
-//			"netbox_address":            dataSourceNetboxAddress(),
-			"netbox_prefixes":           dataSourceNetboxPrefixes(),
-			"netbox_first_free_address": dataSourceNetboxFirstFreeAddress(),
-		},
-
-		ConfigureFunc: providerConfigure,
-	}
-}
-
 var descriptions map[string]string
 
+// Run before to initiliza vars ...
 func init() {
 	descriptions = map[string]string{
 		"app_id":   "The application ID required for API requests",
 		"endpoint": "The full URL (plus path) to the API endpoint",
+		"timeout": "Max. wait time should wait for a successful connection to the API",
 	}
 }
 
+// Provider returns a terraform.ResourceProvider.
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		Schema: providerSchema(),
+		ResourcesMap: providerResources(), 
+		ConfigureFunc: providerConfigure,
+	}
+}
+
+// List of supported configuration fields for your provider.
+// Here we define a linked list of all the fields that we want to
+// support in our provider (api_key, endpoint, timeout & max_retries).
+// More info in https://github.com/hashicorp/terraform/blob/v0.6.6/helper/schema/schema.go#L29-L142
+func providerSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"app_id": &schema.Schema{
+			Type:        schema.TypeString,
+			Optional:    true,
+			Default:     "",
+			Description: descriptions["key created on netbox"],
+		},
+		"endpoint": &schema.Schema{
+			Type:        schema.TypeString,
+			Optional:    true,
+			Default:     "",
+			Description: descriptions["endpoint of netbox (without http:// and / )"],
+		},
+		"timeout": &schema.Schema{
+			Type: schema.TypeInt,
+			Required: false,
+			Description: description["Max. wait time should wait for a successful connection to the API"],
+		}
+	},
+}
+
+
+// List of supported resources and their configuration fields.
+// Here we define da linked list of all the resources that we want to
+// support in our provider. As an example, if you were to write an AWS provider
+// which supported resources like ec2 instances, elastic balancers and things of that sort
+// then this would be the place to declare them.
+// More info here https://github.com/hashicorp/terraform/blob/v0.6.6/helper/schema/resource.go#L17-L81
+func providerResources() map[string] *schema.Resource {
+	return 	map[string]*schema.Resource{
+		    "netbox_prefixes": resourceNetboxPrefixes(),
+		},
+		DataSourcesMap: map[string]*schema.Resource{
+			"netbox_prefixes":           dataSourceNetboxPrefixes(),
+			"netbox_first_free_address": dataSourceNetboxFirstFreeAddress(),
+		},
+
+
+}
+
+
+
+
+// This is the function used to fetch the configuration params given
+// to our provider which we will use to initialise a dummy client that
+// interacts with the API.
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 	log.Printf("[DEBUG] provider.providerConfigure JP provider.providerConfigure APP_ID %s",d.Get("app_id"))
 	log.Printf("[DEBUG] provider.providerConfigure JP provider.providerConfigure ENDPOINT %s",d.Get("endpoint"))
 	config := Config{
 		AppID:    d.Get("app_id").(string),
 		Endpoint: d.Get("endpoint").(string),
+		Timeout: d.Get("timeout").(int),
 	}
 	return config.Client()
 }
