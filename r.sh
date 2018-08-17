d2faf6f8b2ed6b095b4ebd0c99d8acc80fd9aec7
F4955639 Joao Paulo Limberger <f4955639@bb.com.br>
2018-08-17 10:30:34 -0300

    Adjust script to run

--- r.sh
+++ r.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+# Run a sample
 make build
 cp pkg/linux_amd64/terraform-provider-netbox $GOPATH/bin
+mkdir tmp
+export TF_TF_LOG_PATH=tmp/log
+export TF_LOG=DEBUG
 terraform init
 terraform plan
 terraform apply
